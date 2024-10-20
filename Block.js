3d2fc3f5b9291d953aaefe05c3509bb8af632799
iamma00 <mahmoodsam53@gmail.com>
2024-10-20 22:49:48 +0530

    genesis added

--- Block.js
+++ Block.js
@@ -1,3 +1,4 @@
+const { GENESIS_DATA } =  require('./config');
 class Block{
     constructor({timestamp,prevHash,hash,data}){
         this.timestamp =timestamp;
@@ -5,9 +6,14 @@ class Block{
         this.hash = hash;
         this.data = data;
     }
+    static genesis(){
+        return new this(GENESIS_DATA);
+    }
 }
 
 const block1 = new Block({
     timestamp:'2/09/2022',prevHash:'0xabc',hash:'0xc12',data:'hello'
 });
-console.log(block1);
+//console.log(block1);
+const genesisBlock = Block.genesis();
+console.log(genesisBlock);
