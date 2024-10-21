c3600e68be4018572d3403d9f7d2a48a5c9ba88f
iamma00 <mahmoodsam53@gmail.com>
2024-10-21 21:51:16 +0530

    generated hash

--- crypto-hash.js
+++ crypto-hash.js
@@ -1,6 +1,11 @@
+const { log } = require('console');
 const crypto = require('crypto');
 
 const cryptoHash = (...inputs)=>{
     const hash = crypto.createHash('sha256')
-    hash.update(inputs.join)
+    hash.update(inputs.join(''))
+    return hash.digest('hex')
 }
+
+result = cryptoHash("hello", "world")
+console.log(result);
